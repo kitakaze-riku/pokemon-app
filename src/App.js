a75e905a8f5fa508a271f17667460e1efe782a5b
kitakaze-riku <kitakaze.rop@gmail.com>
2023-04-06 17:27:12 +0900

    ✨各ポケモンの詳細データを非同期処理で取得させました。

--- src/App.js
+++ src/App.js
@@ -10,34 +10,39 @@ function App() {
 
   //11.useStateを使ってloadingのbooleanをtrueにしておく。
   const [loading, setLoading] = useState(true);
+  // _pokemonDataをスコープ外でも使いたいためuseStateで管理する。
+  const [pokemonData, setPokemonData] = useState([]);
 
   // ブラウザをリロードしたらポケモンを取得したいのでuseEffect
   useEffect(() => {
-    // 非同期処理を行いたい場合はasync関数を使うとよい
+    // 非同期処理を行いたい場合はasync/await関数を使うこと
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
-
       // 12.各ポケモンの詳細データを取得
       loadPokemon(res.results);
-
       //11.ポケモンデータが取得できたらloadingのstateをfalseに切り替える
       setLoading(false);
     }
-
     fetchPokemonData();
   }, []);
 
-  // 各ポケモンの詳細データが必要なのでjsonデータをもっと細かく分けるための関数
-  const loadPokemon = (data) => {
-    let _pokemonData = Promise.all(
+  //13.各ポケモンの詳細データが必要なのでjsonデータをもっと細かく分けるための関数
+  const loadPokemon = async (data) => {
+    // async/awaitとは非同期処理の構文のこと https://qiita.com/soarflat/items/1a9613e023200bbebcb3
+    let _pokemonData = await Promise.all(
       data.map((pokemon) => {
+        // pokemon.urlでポケモンの詳細を渡している
         let pokemonRecord = getPokemon(pokemon.url);
         return pokemonRecord;
       })
     );
-
+    // useState呼び出して_pokemonDataを格納する。
+    setPokemonData(_pokemonData);
   }
+
+  // useStateを使っているので_pokemonDataがpokemonDataとして出力される。
+  console.log(pokemonData);
 
   return (
     <div className="App">
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,5 +1,7 @@
 export const getAllPokemon = (url) => {
 
+    //メモ 『resolve』とはデータ取得が成功した時
+    // 『reject』とは失敗した時。今回は成功した時の場合のみ記述する。
     // 10.Promiseオブジェクトを使用して取得してきた値をjson形式で取得
     return new Promise((resolve, reject) => {
         // fetchでurlを取得
@@ -12,6 +14,13 @@ export const getAllPokemon = (url) => {
     })
 }
 
-export const getPokemon = () => {
-
+//13.一つ一つのポケモンの詳細データを取得する
+export const getPokemon = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                resolve(data)
+            });
+    });
 };
